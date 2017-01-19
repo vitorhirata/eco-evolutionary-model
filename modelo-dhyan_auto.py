acd160d3360518675d23b13fd46f79d62b487b34
vitor <vitors.hirata@gmail..com>
2017-01-19 18:30:43 -0200

    Make lambda constant and change initial conditions

--- modelo-dhyan_auto.py
+++ modelo-dhyan_auto.py
@@ -7,7 +7,7 @@ b=5.0799
 beta = 5.831E-6
 delta = 1.6369
 lamb = 150.54
-ni = 2480.0 #507.21 # 2480.0
+ni = 507.21 # 507.21 ou 2480.0
 r=348.145
 
 T=.0004
@@ -26,13 +26,11 @@ nc_init = 800
 x0 = array([nc_init])
 
 print(nmenos)
-nmais = ((lamb+b-c)/r + lamb*beta+sqrt(((lamb+b-c)/r + lamb*beta)**2-4*lamb*beta*(lamb+b-c+ni)/r))/(2*lamb*beta/r)
-print(nmais)
 
 def LV(x, t, b, c, beta, ni, lamb, r):
     return array([x[0]*(x[0]/r-1)*(b-c+lamb*(1-x[0]*beta))-ni*x[0] ])
 
-for i in range(0,17):
+for i in range(0,18):
     x0 = array([nc_init*pow(1.5,i)])
     x = odeint(LV, x0, t, (b,c, beta, ni, lamb, r))
     if x0[0] < nmenos:
@@ -54,4 +52,4 @@ plt.xlim(-0.1,t_max)
 plt.xlabel("Dias")
 plt.ylabel("Populacao total")
 plt.title(r"$\nu$ = {}".format(ni))
-#plt.show()
+plt.show()
