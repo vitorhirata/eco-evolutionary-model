a8f66d18a4abbb8e6394d5fc7f528f3b9968af15
vitor <vitors.hirata@gmail..com>
2017-03-03 20:28:43 -0300

    .gitignore is now working

--- integration.jl
+++ integration.jl
@@ -2,8 +2,12 @@ N0=175000/750.0
 gamma=0.439
 T=20.5
 k=1.76e5
+theta=750.0
 
 
+int_N1 = k*log((k+N0*(exp(gamma*T)-1))/k)
+int_N2 = k*N0*(gamma*T/N0+log((N0+(k-N0)*exp(-gamma*T))/k)/N0+1/k-1/(N0+(k-N0)*exp(-gamma*T)))
+int_theta = N0*(theta-1)
 
 
 N(t)=k*N0*exp(gamma*t)/(k+N0*(exp(gamma*t)-1))
@@ -27,9 +31,16 @@ ni = 507.21
 r=348.145
 
 e=2500.0
-T=20.5
+T=1
+
+N= 174993.2
 
 
 
-(N^2*((b+lamb)/r+lamb*beta)+N*c)*T*e
-(lamb*beta*N^3/r+c*N^2/r+N*(b+lamb+ni))*T*e
+(N^2*((b+lamb)/r+lamb*beta)+N*c)*T*e     #nasce
+(lamb*beta*N^3/r+c*N^2/r+N*(b+lamb+ni))*T*e  #morre
+
+
+
+N*(N/r-1)*(b-c+lamb)*T*e
+(N*N*(N/r-1)*beta*lamb+ni*N)*T*e
